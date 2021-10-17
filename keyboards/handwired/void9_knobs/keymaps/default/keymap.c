5c66ba8fcb53648cef9e33dc51f2ecbab7576b23
tomsawdayee <tom.sawdayee@sap.com>
2021-10-18 02:11:41 +0300

    Add void9-knobs

--- keyboards/handwired/void9_knobs/keymaps/default/keymap.c
+++ keyboards/handwired/void9_knobs/keymaps/default/keymap.c
@@ -16,6 +16,8 @@
 #include QMK_KEYBOARD_H
 bool is_alt_tab_active = false;
 bool is_encoder_a_key_active = false;
+bool is_encoder_b_key_active = false;
+
 uint16_t alt_tab_timer = 0;
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
@@ -26,8 +28,9 @@ enum layer_names {
 };
 
 enum custom_keycodes {
-    PASS = SAFE_RANGE,
-    ENCODER_B_PRESS
+    ENCODER_A_PRESS,
+    ENCODER_B_PRESS,
+    TD_PASS = 1
 };
 
 
@@ -43,13 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `--------------------'
  */
 [_BASE] = LAYOUT_ortho_3x3(
-    KC_7,       LALT(LCMD(KC_LEFT)), LALT(LCMD(KC_RIGHT)),
+    LCMD(KC_V),       LCMD(KC_C), LCMD(KC_X),
     KC_4,       LCTL(LCMD(KC_Q)),       KC_6,
-    KC__MUTE,       PASS,       ENCODER_B_PRESS
+    ENCODER_B_PRESS,       TD(TD_PASS),       ENCODER_A_PRESS
 ),
 
 /* FN1
- * ,--------------------.
+ * ,--------------------.KC_2
  * |   7  |   8  |   9  |
  * |------+------+------|
  * |   4  |   5  |   6  |
@@ -60,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN1] = LAYOUT_ortho_3x3(
     KC_7,       KC_8,       KC_9,
     KC_4,       KC_5,       KC_6,
-    PASS,       KC_2,       KC_3
+    KC_2,       KC_2,       KC_3
 ),
 
 /* FN2
@@ -117,21 +120,37 @@ void matrix_scan_user(void) {
   }
 }
 
+void td_parenthesis (qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        SEND_STRING ("test");
+        reset_tap_dance (state);
+    }
+    else if (state->count == 2) {
+        SEND_STRING ("test");
+        tap_code16(KC_ENTER);
+    }
+}
+
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_PASS] = ACTION_TAP_DANCE_FN(td_parenthesis),
+};
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case PASS:
-        if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
-            SEND_STRING("test");
-        } else {
-            // when keycode QMKBEST is released
-        }
-        break;
-      case ENCODER_B_PRESS:
+      case ENCODER_A_PRESS:
             if (record->event.pressed) {
                 is_encoder_a_key_active = true;
             } else {
                 is_encoder_a_key_active = false;
+            }
+            break;
+      case ENCODER_B_PRESS:
+            if (record->event.pressed) {
+                is_encoder_b_key_active = true;
+            } else {
+                is_encoder_b_key_active = false;
             }
             break;
         }
@@ -140,28 +159,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {
-      if (clockwise) {
-        if (!is_alt_tab_active) {
-          is_alt_tab_active = true;
-          register_code(KC_LCMD);
-        }
-        alt_tab_timer = timer_read();
-        tap_code16(KC_TAB);
+    if (is_encoder_b_key_active) {
+          if (clockwise) {
+            if (!is_alt_tab_active) {
+              is_alt_tab_active = true;
+              register_code(KC_LCMD);
+            }
+            alt_tab_timer = timer_read();
+            tap_code16(KC_TAB);
+          } else {
+            if (!is_alt_tab_active) {
+              is_alt_tab_active = true;
+              register_code(KC_LCMD);
+            }
+            alt_tab_timer = timer_read();
+            tap_code16(LSFT(KC_TAB));
+          }
       } else {
-        if (!is_alt_tab_active) {
-          is_alt_tab_active = true;
-          register_code(KC_LCMD);
+        if (clockwise) {
+            tap_code16(LALT(LCMD(KC_RIGHT)));
+        } else {
+            tap_code16(LALT(LCMD(KC_LEFT)));
         }
-        alt_tab_timer = timer_read();
-        tap_code16(LSFT(KC_TAB));
       }
   } else {
     if (is_encoder_a_key_active) {
-        if (clockwise) {
-            tap_code16(LCMD(KC_PPLS));
-        } else {
-            tap_code16(LCMD(KC_PMNS));
-        }
+          if (clockwise) {
+              tap_code16(LCMD(KC_PPLS));
+          } else {
+              tap_code16(LCMD(KC_PMNS));
+          }
+
     } else {
         if (clockwise) {
           tap_code16(LCMD(LSFT(KC_Z)));
